40d1338e28272844b755f593aeeab370f43866cb
Austin Edwards <austintedwards@gmail.com>
2017-04-02 12:02:09 -0600

    quotes

---
+++ src/data/quotes.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    category: 'inspirational',
+    quotes: [
+      {
+        id: '1',
+        person: 'Teddy',
+        text: 'Believe you can youre halfway there'
+      },
+      {
+        id: '2',
+        person: 'Norm',
+        text: 'Change you thoughts and you change your world'
+      }
+    ],
+    icon: 'brush'
+  }
+]
